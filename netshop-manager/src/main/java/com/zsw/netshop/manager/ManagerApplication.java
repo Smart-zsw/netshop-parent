265e608d67f806e913499f3bf92ae697614b44f3
zsw <2689498471@qq.com>
2023-11-30 16:38:03 +0800

    修复swagger问题，覆盖默认包的扫描路径，使创建的api分组生效。

--- netshop-manager/src/main/java/com/zsw/netshop/manager/ManagerApplication.java
+++ netshop-manager/src/main/java/com/zsw/netshop/manager/ManagerApplication.java
@@ -2,8 +2,10 @@ package com.zsw.netshop.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.zsw.netshop"})
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class,args);
