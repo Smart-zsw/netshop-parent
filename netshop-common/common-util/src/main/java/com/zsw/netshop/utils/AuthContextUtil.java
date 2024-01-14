39a28a502f2cabcd88f0d1e907f01c8d87e17a82
SHUWEN ZHENG <2689498471@qq.com>
2024-01-14 16:25:50 +0800

    增加工具类AuthContextUtil,在工具类中操作ThreadLocal，增加拦截器类LoginAuthInterceptor,并在在配置类中注册拦截器。

---
+++ netshop-common/common-util/src/main/java/com/zsw/netshop/utils/AuthContextUtil.java
@@ -0,0 +1,24 @@
+package com.zsw.netshop.utils;
+
+import com.zsw.netshop.model.entity.system.SysUser;
+
+public class AuthContextUtil {
+
+    //创建ThreadLocal对象
+    private static final ThreadLocal<SysUser> threadLocal = new ThreadLocal<>();
+
+    //添加数据
+    public static void set(SysUser sysUser) {
+        threadLocal.set(sysUser);
+    }
+
+    //获取数据
+    public static SysUser get() {
+        return threadLocal.get();
+    }
+
+    //删除数据
+    public static void remove() {
+        threadLocal.remove();
+    }
+}
