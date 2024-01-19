fb6122151d067277aa5e3bc414360c4818cd4987
SHUWEN ZHENG <2689498471@qq.com>
2024-01-19 15:34:15 +0800

    角色创建准备： controller service mapper

---
+++ netshop-manager/src/main/java/com/zsw/netshop/manager/controller/SysRoleController.java
@@ -0,0 +1,9 @@
+package com.zsw.netshop.manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/admin/system/sysRole")
+public class SysRoleController {
+}
---
+++ netshop-manager/src/main/java/com/zsw/netshop/manager/service/impl/SysRoleServiceImpl.java
@@ -0,0 +1,8 @@
+package com.zsw.netshop.manager.service.impl;
+
+import com.zsw.netshop.manager.service.SysRoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SysRoleServiceImpl implements SysRoleService {
+}
