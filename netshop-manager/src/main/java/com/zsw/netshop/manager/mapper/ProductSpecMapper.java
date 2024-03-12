39366fdcae7b2f29302db3cedb7504086e02dc96
SHUWEN ZHENG <2689498471@qq.com>
2024-03-12 20:28:01 +0800

    实现商品规格管理中的列表、添加、修改和删除接口。

---
+++ netshop-manager/src/main/java/com/zsw/netshop/manager/mapper/ProductSpecMapper.java
@@ -0,0 +1,22 @@
+package com.zsw.netshop.manager.mapper;
+
+import com.zsw.netshop.model.entity.product.ProductSpec;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ProductSpecMapper {
+
+    //列表
+    List<ProductSpec> findByPage();
+
+    //添加
+    void save(ProductSpec productSpec);
+
+    //修改
+    void update(ProductSpec productSpec);
+
+    //修改
+    void delete(Long id);
+}
